27988dea453d3177b431a13778d2831da9623b70
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2021-06-16 00:51:11 -0400

    Globals File added

---
+++ app/src/main/java/io/gonative/android/Globals.java
@@ -0,0 +1,14 @@
+package io.gonative.android;
+
+import com.facebook.AccessToken;
+
+public class Globals {
+    public static String FacebookTokenString = "";
+    public static AccessToken FacebookToken;
+    public static String UserId = "";
+    public static String FirstName = "";
+    public static String LastName = "";
+    public static String UserEmail = "";
+    public static String UserPictureUrl = "";
+    public static String UserName = "";
+}
