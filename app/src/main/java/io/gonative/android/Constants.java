34da3a11708ae36dfe32ce9f736cd58d42d7fd33
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2021-06-16 00:51:02 -0400

    Constants file added

---
+++ app/src/main/java/io/gonative/android/Constants.java
@@ -0,0 +1,15 @@
+package io.gonative.android;
+
+public final class Constants {
+    public final static String FACEBOOK_EMAIL_PERMISSION = "email";
+    public final static String FACEBOOK_PUBLIC_PROFILE_PERMISSION = "public_profile";
+    public final static String FACEBOOK_ADS_MANAGEMENT_PERMISSION = "ads_management";
+    public final static String FACEBOOK_BUSINESS_MANAGEMENT_PERMISSION = "business_management";
+    public final static String FACEBOOK_INSTAGRAM_BASIC_PERMISSION = "instagram_basic";
+    public final static String FACEBOOK_INSTAGRAM_CONTENT_PUBLISH_PERMISSION = "instagram_content_publish";
+    public final static String FACEBOOK_PAGES_READ_ENGAGEMENT_PERMISSION = "pages_read_engagement";
+    public final static String FACEBOOK_PAGES_MANAGE_POSTS_PERMISSION = "pages_manage_posts";
+    public final static String FACEBOOK_INSTAGRAM_MANAGE_COMMENTS_PERMISSION = "instagram_manage_comments";
+    public final static String FACEBOOK_PAGES_SHOW_LIST_PERMISSION = "pages_show_list";
+    public final static String FACEBOOK_PUBLISH_VIDEO_PERMISSION = "publish_video";
+}
